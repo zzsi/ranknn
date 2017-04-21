9b6b703e69992842c585b268fa7a30b5b12685b1
ZZ <zhangzhang.si@gmail.com>
2017-04-21 09:31:17 -0500

    rename to ranknn

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='rankpy',
+setup(name='ranknn',
       version='0.1',
-      description='Learning to Rank',
+      description='Learning to Rank with Keras and Tensorflow',
       url='',
       author='ZZ',
       author_email='',
       license='MIT',
-      packages=['rankpy'],
+      packages=['ranknn'],
       install_requires=[
       	'tensorflow>=1',
       	'keras>=2',
